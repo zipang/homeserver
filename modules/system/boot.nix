841af450de1a4636a6fa5c8e9475465347c2691e
zipang <christophe.desguez@gmail.com>
2026-01-15 17:17:39 +0100

    feat: limit boot menu entries to 6 to prevent clutter

--- modules/system/boot.nix
+++ modules/system/boot.nix
@@ -3,5 +3,7 @@
 {
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
+  # Keep only the 6 last boot entries in the grub menu
+  boot.loader.systemd-boot.configurationLimit = 6;
   boot.loader.efi.canTouchEfiVariables = true;
 }
