2bfb573c68f035d5bd5c640b41621a2cefc9c7a9
zipang <christophe.desguez@gmail.com>
2026-01-28 15:55:49 +0100

    chore: temporarily disable Authelia and sops secret to allow openssl installation

--- modules/services/nginx.nix
+++ modules/services/nginx.nix
@@ -15,15 +15,15 @@
     clientMaxBodySize = "10G";
 
     # Virtual Hosts will be added as we implement services
-    virtualHosts."auth.skylab.local" = {
-      forceSSL = true;
-      sslCertificate = "/var/lib/secrets/certs/skylab.crt";
-      sslCertificateKey = "/var/lib/secrets/certs/skylab.key";
-      locations."/" = {
-        proxyPass = "http://127.0.0.1:9091";
-        proxyWebsockets = true;
-      };
-    };
+    # virtualHosts."auth.skylab.local" = {
+    #   forceSSL = true;
+    #   sslCertificate = "/var/lib/secrets/certs/skylab.crt";
+    #   sslCertificateKey = "/var/lib/secrets/certs/skylab.key";
+    #   locations."/" = {
+    #     proxyPass = "http://127.0.0.1:9091";
+    #     proxyWebsockets = true;
+    #   };
+    # };
 
     virtualHosts."syncthing.skylab.local" = {
       forceSSL = true;
@@ -33,38 +33,38 @@
       locations."/" = {
         proxyPass = "http://127.0.0.1:8384";
         proxyWebsockets = true;
-        extraConfig = ''
-          auth_request /authelia;
-          auth_request_set $target_url $scheme://$http_host$request_uri;
-          error_page 401 = &https://auth.skylab.local/?rd=$target_url;
-          
-          # Pass user information to the backend
-          auth_request_set $user $upstream_http_remote_user;
-          auth_request_set $groups $upstream_http_remote_groups;
-          auth_request_set $name $upstream_http_remote_name;
-          auth_request_set $email $upstream_http_remote_email;
-          proxy_set_header Remote-User $user;
-          proxy_set_header Remote-Groups $groups;
-          proxy_set_header Remote-Name $name;
-          proxy_set_header Remote-Email $email;
-        '';
+        # extraConfig = ''
+        #   auth_request /authelia;
+        #   auth_request_set $target_url $scheme://$http_host$request_uri;
+        #   error_page 401 = &https://auth.skylab.local/?rd=$target_url;
+        #   
+        #   # Pass user information to the backend
+        #   auth_request_set $user $upstream_http_remote_user;
+        #   auth_request_set $groups $upstream_http_remote_groups;
+        #   auth_request_set $name $upstream_http_remote_name;
+        #   auth_request_set $email $upstream_http_remote_email;
+        #   proxy_set_header Remote-User $user;
+        #   proxy_set_header Remote-Groups $groups;
+        #   proxy_set_header Remote-Name $name;
+        #   proxy_set_header Remote-Email $email;
+        # '';
       };
 
-      locations."/authelia" = {
-        proxyPass = "http://127.0.0.1:9091/api/verify";
-        extraConfig = ''
-          internal;
-          proxy_set_header Host auth.skylab.local;
-          proxy_set_header X-Original-URL $scheme://$http_host$request_uri;
-          proxy_set_header X-Forwarded-Method $request_method;
-          proxy_set_header X-Forwarded-Proto $scheme;
-          proxy_set_header X-Forwarded-Host $http_host;
-          proxy_set_header X-Forwarded-Uri $request_uri;
-          proxy_set_header X-Forwarded-For $remote_addr;
-          proxy_pass_request_body off;
-          proxy_set_header Content-Length "";
-        '';
-      };
+      # locations."/authelia" = {
+      #   proxyPass = "http://127.0.0.1:9091/api/verify";
+      #   extraConfig = ''
+      #     internal;
+      #     proxy_set_header Host auth.skylab.local;
+      #     proxy_set_header X-Original-URL $scheme://$http_host$request_uri;
+      #     proxy_set_header X-Forwarded-Method $request_method;
+      #     proxy_set_header X-Forwarded-Proto $scheme;
+      #     proxy_set_header X-Forwarded-Host $http_host;
+      #     proxy_set_header X-Forwarded-Uri $request_uri;
+      #     proxy_set_header X-Forwarded-For $remote_addr;
+      #     proxy_pass_request_body off;
+      #     proxy_set_header Content-Length "";
+      #   '';
+      # };
     };
   };
 
